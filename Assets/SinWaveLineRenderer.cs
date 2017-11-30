f66c2a02af5ba7666f9f41c6ff2b54c0ffe2412a
Freakadelle <doehler.robert@gmail.com>
2017-11-30 16:02:48 +0100

    final

---
+++ Assets/SinWaveLineRenderer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinWaveLineRenderer : MonoBehaviour
+{
+    public Gradient lineColorGradient;
+    public int interpolationDetail = 20;
+
+    [Range(0.0001f,1)]
+    public float waveLength;
+    [Range(0,10)]
+    public float frequency;
+
+    public Material lineMaterial;
+
+    public AnimationCurve amplitudeOverTime;
+    public AnimationCurve follow2DPathCurve;
+
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = lineMaterial;
+        lineRenderer.colorGradient = lineColorGradient;
+        lineRenderer.startWidth = 0.2f;
+        lineRenderer.endWidth = 0.2f;
+        lineRenderer.positionCount = interpolationDetail;
+    }
+
+    void Update()
+    {
+
+        lineRenderer.positionCount = interpolationDetail;
+
+        int i = 0;
+        while (i < lineRenderer.positionCount)
+        {
+            float interpolation = (i / (float)interpolationDetail);
+
+            float amplitude = transform.localScale.y * amplitudeOverTime.Evaluate(interpolation);
+            float followPath = follow2DPathCurve.Evaluate(interpolation);
+
+            //float sinusX = (Time.time / periodTime) - (interpolation * Mathf.PI * 2 / waveLength);
+            float sinusX = (2 * Mathf.PI * frequency * Time.time) + ((interpolation * Mathf.PI * 2) / (waveLength));
+            float xPos = (interpolation * transform.localScale.x) + transform.position.x;
+            float yPos = transform.position.y + followPath;
+
+            Vector3 pos = new Vector3(xPos, amplitude * Mathf.Sin(sinusX) + yPos, transform.position.z);
+            lineRenderer.SetPosition(i, pos);
+            i++;
+        }
+    }
+}
